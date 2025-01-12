47838daa8177553f8e8cac09654481bba7c812ad
anggi.anggoro26@gmail.com <anggi.anggoro26@gmail.com>
2025-01-12 16:01:44 +0700

    Fixing Cache

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,34 @@ import type { NextConfig } from "next";
 const withPWA = require('next-pwa')({
   dest: 'public', 
   register: true, 
+  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
   sw:'sw.js',
   scope: "/app",
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/customer-w-anggi\.vercel\.app\//,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'api-cache',
+        expiration: {
+          maxEntries: 30,
+          maxAgeSeconds: 60 * 30, 
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/restcountries\.com\/v3\.1\/all\?fields=name$/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'api-cache',
+        expiration: {
+          maxEntries: 30,
+          maxAgeSeconds:  60 * 30, 
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig: NextConfig = {
