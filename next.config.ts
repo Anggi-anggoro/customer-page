c034c7c9a33d1cd42a938c2e6d3db24c4cab3352
anggi.anggoro26@gmail.com <anggi.anggoro26@gmail.com>
2025-01-13 11:24:03 +0700

    Add token expired

--- next.config.ts
+++ next.config.ts
@@ -1,49 +1,49 @@
-import type { NextConfig } from "next";
+  import type { NextConfig } from "next";
 
-const withPWA = require('next-pwa')({
-  dest: 'public', 
-  register: true, 
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  scope: "/app",
-  sw : '/public/sw.js',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/customer-w-anggi\.vercel\.app\//,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'api-cache',
-        expiration: {
-          maxEntries: 30,
-          maxAgeSeconds: 60 * 30, 
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/restcountries\.com\/v3\.1\/all\?fields=name$/,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'api-cache',
-        expiration: {
-          maxEntries: 30,
-          maxAgeSeconds:  60 * 30, 
-        },
-      },
-    },
-  ],
-});
-
-const nextConfig: NextConfig = {
-  images : {
-    formats :['image/webp'],
-    remotePatterns : [
+  const withPWA = require('next-pwa')({
+    dest: 'public', 
+    register: true, 
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    scope: "/",
+    sw : '/sw.js',
+    runtimeCaching: [
       {
-        protocol : 'https',
-        hostname : 'wwwyguzvcriyafipdond.supabase.co',
-        pathname : '/storage/v1/**/public/photo/**'
-      }
-    ]
-  },
-};
+        urlPattern: /^https:\/\/customer-w-anggi\.vercel\.app\//,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'api-cache',
+          expiration: {
+            maxEntries: 30,
+            maxAgeSeconds: 60 * 30, 
+          },
+        },
+      },
+      {
+        urlPattern: /^https:\/\/restcountries\.com\/v3\.1\/all\?fields=name$/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'api-cache',
+          expiration: {
+            maxEntries: 30,
+            maxAgeSeconds:  60 * 30, 
+          },
+        },
+      },
+    ],
+  });
 
-export default withPWA(nextConfig)  ;
+  const nextConfig: NextConfig = {
+    images : {
+      formats :['image/webp'],
+      remotePatterns : [
+        {
+          protocol : 'https',
+          hostname : 'wwwyguzvcriyafipdond.supabase.co',
+          pathname : '/storage/v1/**/public/photo/**'
+        }
+      ]
+    },
+  };
+
+  export default withPWA(nextConfig)  ;
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -15,7 +15,7 @@ export const createClient = async () => {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => {
-              cookieStore.set(name, value, options);
+              cookieStore.set(name, value, {...options, maxAge : 60 * 60});
             });
           } catch (error) {
             // The `set` method was called from a Server Component.
